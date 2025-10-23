7dca8442407412ddff4cd46003b39c1b6bdbf805
daniel050504 <adaw25daguado@inslaferreria.cat>
2025-10-23 15:26:04 +0200

    Primera version del Projecto

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Hola món amb PHP</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <h1><?php echo "Hola món des de PHP i Docker!"; ?></h1>
+</body>
+</html>
