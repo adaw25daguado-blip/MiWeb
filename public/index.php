09b7585589ae902042710fec359865816ec7089e
daniel050504 <adaw25daguado@inslaferreria.cat>
2025-10-23 15:55:48 +0200

    Paso 9

--- public/index.php
+++ public/index.php
@@ -7,5 +7,21 @@
 </head>
 <body>
   <h1><?php echo "Hola món des de PHP i Docker!"; ?></h1>
+
+  <?php
+    $servername = "db";
+    $username = "user";
+    $password = "password";
+    $database = "demo";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("❌ Connexió fallida: " . $conn->connect_error);
+    }
+    echo "✅ Connexió correcta a la base de dades MySQL!";
+    $conn->close();
+  ?>
+
 </body>
 </html>
